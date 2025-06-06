cb30ae544a77182b97ca4765abbeeaef4a6c4fc3
hi-lufz1 <latifufauzi@gmail.com>
2025-06-06 20:50:15 +0700

    membuat state profile buyer dan bloc proifle buyer

--- lib/presentation/buyer/profile/bloc/profile_buyer_bloc.dart
+++ lib/presentation/buyer/profile/bloc/profile_buyer_bloc.dart
@@ -1,14 +1,43 @@
 import 'package:bloc/bloc.dart';
 import 'package:canaryfarm/data/model/response/buyer/buyer_profile_response_model.dart';
+import 'package:canaryfarm/data/repository/profile_buyer_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'profile_buyer_event.dart';
 part 'profile_buyer_state.dart';
 
 class ProfileBuyerBloc extends Bloc<ProfileBuyerEvent, ProfileBuyerState> {
-  ProfileBuyerBloc() : super(ProfileBuyerInitial()) {
-    on<ProfileBuyerEvent>((event, emit) {
-      // TODO: implement event handler
+  final ProfileBuyerRepository profileBuyerRepository;
+  ProfileBuyerBloc({required this.profileBuyerRepository})
+    : super(ProfileBuyerInitial()) {
+    on<AddProfileBuyerEvent>(_addProfileBuyer);
+    on<GetProfileBuyerEvent>(_getProfileBuyer);
+  }
+
+  Future<void> _addProfileBuyer(
+    AddProfileBuyerEvent event,
+    Emitter<ProfileBuyerState> emit,
+  ) async {
+    emit(ProfileBuyerLoading());
+    final result = await profileBuyerRepository.addProfileBuyer(
+      event.requestModel,
+    );
+    result.fold((error) => emit(ProfileBuyerAddError(message: error)), (
+      profile,
+    ) {
+      emit(ProfileBuyerAdded(profile: profile));
     });
+  }
+
+  Future<void> _getProfileBuyer(
+    GetProfileBuyerEvent event,
+    Emitter<ProfileBuyerState> emit,
+  ) async {
+    emit(ProfileBuyerLoading());
+    final result = await profileBuyerRepository.getProfileBuyer();
+    result.fold(
+      (error) => emit(ProfileBuyerError(message: error)),
+      (profile) => emit(ProfileBuyerLoaded(profile: profile)),
+    );
   }
 }
